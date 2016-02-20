98cc697df3d54f0e7dc6b895fb3449b6aab042d4
Riley Spicer <riley.r.spicer@gmail.com>
2016-02-20 13:28:06 -0800

    fixed test database

--- spec/models/pantry_item_spec.rb
+++ spec/models/pantry_item_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe PantryItem, type: :model do
 
   describe ".validates" do
     it "must have a name" do
-      expect(create(:pantry_item)).to be_valid
+      expect(build(:pantry_item)).to be_valid
       expect(build(:pantry_item, name: nil)).to be_invalid
     end
   end
