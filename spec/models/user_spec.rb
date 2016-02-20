87692d26bcdb5ba7ba397f3e9a78757f8d738942
Riley Spicer <riley.r.spicer@gmail.com>
2016-02-20 13:32:14 -0800

    email uniqueness tested

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,6 +7,10 @@ RSpec.describe User, type: :model do
       expect(build(:user)).to be_valid
       expect(build(:user, email: nil)).to be_invalid
     end
+    it "must have unique email" do
+      user = create(:user)
+      expect(build(:user)).to be_invalid
+    end
   end
 
 end
