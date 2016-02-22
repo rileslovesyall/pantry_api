a9055d82734579566e03c332f5bd472a0a82c30b
Riley Spicer <riley.r.spicer@gmail.com>
2016-02-21 16:58:26 -0800

    added validations

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,7 @@ class Category < ActiveRecord::Base
   has_many :pantry_item_categories
   has_many :pantry_items, through: :pantry_item_categories
 
+  validates_presence_of :name
+  validates_uniqueness_of :name
+
 end
