4a848b944166e71b4f001388afd3dd64334e767a
Riley Spicer <riley.r.spicer@gmail.com>
2016-02-25 10:21:15 -0800

    added factory associations

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,14 +1,23 @@
 FactoryGirl.define do
   
+  factory :user1, class: User do
+    name "Beth"
+    email "horse@surgeon.com"
+    password "123"
+    password_confirmation "123"
+  end
+
+  factory :user2, class: User do
+    name "Jerry"
+    email "jerry@cloudatlas.com"
+    password "123"
+    password_confirmation "123"
+  end
+
   factory :pantry_item, class: PantryItem do
     name "Pickled Parakeets"
     quantity 3
-    user_id 3
-  end
-
-  factory :user do
-    name "Riley"
-    email "abc@abc.com"
+    association :user, factory: :user1
   end
 
 end
