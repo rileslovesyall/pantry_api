2737da3872f7c3e367b03cc32b8da10a921303f6
Riley Spicer <riley.r.spicer@gmail.com>
2016-03-08 10:04:40 -0800

    minor change in how to send back expiring soon items

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -94,9 +94,9 @@ module Pantry
         expiring_soon = lambda do
           requester_must_be_user
           exp = PantryItemsUser.expiring_soon(@curr_user.id)
-          exp_items = {}
+          exp_items = []
           exp.each do |e|
-            exp_items[:exp_item] = {exp: e['exp_date'], pantry_item: PantryItem.find(e['pantry_item_id'].to_i)}
+            exp_items << PantryItem.find(e['pantry_item_id'].to_i)
           end
           status 200
           return exp_items.to_json
