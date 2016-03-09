418834c46f656ee139e983e3e767d44f5e3eea11
Riley Spicer <riley.r.spicer@gmail.com>
2016-03-09 12:30:43 -0800

    skeletoned specs

--- spec/models/pantry_items_users_spec.rb
+++ spec/models/pantry_items_users_spec.rb
@@ -2,5 +2,22 @@ require 'spec_helper'
 
 RSpec.describe PantryItemsUser, type: :model do
 
+  describe ".expiring_soon(user_id)" do
+    it "returns pantry_items belonging to that user" do
+      
+    end
+    it "returns items with expiration dates between now and the number of days set by the user" do
+      
+    end
+  end
+
+  describe ".consume(pantry_item_id, quant)" do
+    context "when there are enough of this item to consume" do
+      
+    end
+    context "when there isn't enough of this item to consume" do
+      expect{PantryItemsUser.consume()}.to raise_error("You don't have enough of this item to consume!")
+    end
+  end
 
 end
